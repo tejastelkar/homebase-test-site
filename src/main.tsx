427689d58cfaea3f5a8498ecae4ddcfbcd4ba053
Tejas Telkar <tejastelkar9@gmail.com>
2026-01-14 16:37:00 +0530

    fix: react mount and router basename

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
 import "./index.css";
 
-const basename = import.meta.env.PROD ? "/tejas-homebase" : "/";
-
-ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-).render(
-  <React.StrictMode>
-    <BrowserRouter basename={basename}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+createRoot(document.getElementById("root")!).render(<App />);
